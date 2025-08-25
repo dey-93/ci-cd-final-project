24e4588c43801612b4c1e8ca6a67815008c6738c
dey-93 <daquino@eglobal.com.mx>
2025-08-25 19:50:07 -0400

    test: paquete app y prueba mínima + nose-spec/colorama para --with-spec

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,5 @@
-def suma(a, b):
-    return a + b
+try:
+    from service import *  # reexporta si tienes paquete service
+except Exception:
+    def suma(a, b):  # fallback si no hay service
+        return a + b
